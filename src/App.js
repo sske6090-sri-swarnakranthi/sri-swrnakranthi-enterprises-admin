ca0ab443fd9ddbbdc01f4b27b9658aa112e462ca
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-01-28 10:43:27 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider, useAuth } from './pages/AdminAuth'
+import { AuthProvider } from './pages/AdminAuth'
 import { LoadingProvider } from './pages/LoadingContext'
 import HomePage from './pages/HomePage'
 import Transaction from './pages/Transaction'
 import Stocks from './pages/Stocks'
 import Sales from './pages/Sales'
 import Customers from './pages/Customers'
-import LoginAdmin from './pages/LoginAdmin'
 import ImportStock from './pages/ImportStock'
 import POS from './pages/POS'
 import AdminHomepageImages from './pages/AdminHomepageImages'
 import OrderIssues from './pages/OrderIssues'
 import ReturnReview from './pages/ReturnReview'
-
-function RequireAuth({ children }) {
-  const { token } = useAuth()
-  if (!token) return <Navigate to="/login" replace />
-  return children
-}
+import LoginAdmin from './pages/LoginAdmin'
 
 export default function App() {
   return (
@@ -27,88 +21,16 @@ export default function App() {
         <Router>
           <Routes>
             <Route path="/login" element={<LoginAdmin />} />
-
-            <Route
-              path="/"
-              element={
-                <RequireAuth>
-                  <HomePage />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/transactions"
-              element={
-                <RequireAuth>
-                  <Transaction />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/stocks"
-              element={
-                <RequireAuth>
-                  <Stocks />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/sales"
-              element={
-                <RequireAuth>
-                  <Sales />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/customers"
-              element={
-                <RequireAuth>
-                  <Customers />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/pos"
-              element={
-                <RequireAuth>
-                  <POS />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/import"
-              element={
-                <RequireAuth>
-                  <ImportStock />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/homepage-images"
-              element={
-                <RequireAuth>
-                  <AdminHomepageImages />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/order-issues"
-              element={
-                <RequireAuth>
-                  <OrderIssues />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/returns/:id"
-              element={
-                <RequireAuth>
-                  <ReturnReview />
-                </RequireAuth>
-              }
-            />
-
+            <Route path="/" element={<HomePage />} />
+            <Route path="/transactions" element={<Transaction />} />
+            <Route path="/stocks" element={<Stocks />} />
+            <Route path="/sales" element={<Sales />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/pos" element={<POS />} />
+            <Route path="/import" element={<ImportStock />} />
+            <Route path="/homepage-images" element={<AdminHomepageImages />} />
+            <Route path="/order-issues" element={<OrderIssues />} />
+            <Route path="/returns/:id" element={<ReturnReview />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
